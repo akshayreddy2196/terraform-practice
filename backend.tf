bf6cab28579bc47b600f6f5820828b3d09b7232f
akshayreddy2196 <77155965+akshayreddy2196@users.noreply.github.com>
2025-09-02 10:45:35 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "devops-tfstate"
+    bucket         = "devops-tfstate-hu1"
     key            = "dev/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
